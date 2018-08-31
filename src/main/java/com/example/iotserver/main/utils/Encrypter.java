cdb5ad7b96dbedb51fb8c31ada6b69927fb30230
Kamil <kamilbajdo@gmail.com>
2018-08-31 13:40:35 +0200

    Updated encrypter to java 9 and higher

--- src/main/java/com/example/iotserver/main/utils/Encrypter.java
+++ src/main/java/com/example/iotserver/main/utils/Encrypter.java
@@ -1,6 +1,5 @@
 package com.example.iotserver.main.utils;
 
-import javax.xml.bind.DatatypeConverter;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
@@ -9,8 +8,12 @@ public class Encrypter {
     public static String encrypt(String password) throws Exception{
         MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
         byte[] digest = messageDigest.digest(password.getBytes(StandardCharsets.UTF_8));
-        String encrypted = DatatypeConverter.printHexBinary(digest).toLowerCase();
-
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i< digest.length ;i++)
+        {
+            sb.append(Integer.toString((digest[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        String encrypted = sb.toString();
         return encrypted;
     }
 }
