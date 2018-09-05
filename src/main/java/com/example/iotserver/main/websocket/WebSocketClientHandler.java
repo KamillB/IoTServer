3e97c85d4f47d0bd75fdf9c02e1ecc9f746a80ac
Kamil <kamilbajdo@gmail.com>
2018-09-05 16:40:48 +0200

    Added peripheries to data that is sent. Tried to make runnable that updates user data but not works yet

--- src/main/java/com/example/iotserver/main/websocket/WebSocketClientHandler.java
+++ src/main/java/com/example/iotserver/main/websocket/WebSocketClientHandler.java
@@ -1,12 +1,19 @@
 package com.example.iotserver.main.websocket;
 
 
-import com.example.iotserver.main.models.ClientData;
+import ch.qos.logback.core.net.server.Client;
+import com.example.iotserver.main.models.other.ClientData;
 import com.example.iotserver.main.models.db.*;
 import com.example.iotserver.main.models.dbModels.*;
+import com.example.iotserver.main.models.other.RpiMessage;
 import com.example.iotserver.main.models.wsModels.*;
 import com.example.iotserver.main.repository.*;
 import com.google.gson.Gson;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.tomcat.jni.Time;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
@@ -16,6 +23,9 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class WebSocketClientHandler extends TextWebSocketHandler {
@@ -32,10 +42,15 @@ public class WebSocketClientHandler extends TextWebSocketHandler {
     TemperatureRepository temperatureRepository;
     @Autowired
     ImageRepository imageRepository;
+    @Autowired
+    PeripheryRepository peripheryRepository;
+    @Autowired
+    TemperatureArchiveRepository temperatureArchiveRepository;
 
     private Gson gson = new Gson();
     private List<WebSocketSession> sessions = new ArrayList<>();
     private Map<WebSocketSession, ClientData> sessionClientDataList = new HashMap<WebSocketSession, ClientData>();
+    private ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws InterruptedException, IOException {
@@ -64,6 +79,15 @@ public class WebSocketClientHandler extends TextWebSocketHandler {
                     System.out.println("Device key is " + deviceKey);
                     addDeviceToUser(session, deviceKey);
                     break;
+
+                case "periphery" :
+                    PeripheryModel periphery = wsMessage.getPayload().getPeripheryModels().get(0);
+                    switchPeripheryStatus(periphery);
+                    break;
+
+                case "temperatureArchive" :
+                    getTemperatureArchiveData(session, wsMessage.getPayload().getTemperatureModel().get(0));
+                    break;
             }
         }
         catch (Exception e){
@@ -75,7 +99,7 @@ public class WebSocketClientHandler extends TextWebSocketHandler {
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-
+        //executor.scheduleAtFixedRate(checkIfDatabaseRecordChanged, 0, 5, TimeUnit.SECONDS);
 
         //////////////////////
         sessions.add(session);
@@ -84,7 +108,7 @@ public class WebSocketClientHandler extends TextWebSocketHandler {
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-
+        //executor.shutdown();
 
         //////////////////////
         sessions.remove(session);
@@ -112,9 +136,10 @@ public class WebSocketClientHandler extends TextWebSocketHandler {
             }
         }
 
-        // Temperature and Image list for user
+        // Temperature, Image and Peripheries list for user
         List<Temperature> userTemperatures = new ArrayList<>();
         List<Image> userImages = new ArrayList<>();
+        List<Periphery> userPeripheries = new ArrayList<>();
         for (Device dev: userDevices) {
             Iterable<Temperature> temp = temperatureRepository.findAllByOwner(dev.getSerialNumber());
             for (Temperature t : temp) {
@@ -125,6 +150,11 @@ public class WebSocketClientHandler extends TextWebSocketHandler {
             for (Image i : img){
                 userImages.add(i);
             }
+
+            Iterable<Periphery> periph = peripheryRepository.findAllByOwner(dev.getSerialNumber());
+            for (Periphery p : periph){
+                userPeripheries.add(p);
+            }
         }
 
         // Create logged in user model for faster handling
@@ -132,7 +162,8 @@ public class WebSocketClientHandler extends TextWebSocketHandler {
                 user,
                 userDevices,
                 userTemperatures,
-                userImages
+                userImages,
+                userPeripheries
         );
         sessionClientDataList.put(session, clientData);
     }
@@ -182,9 +213,7 @@ public class WebSocketClientHandler extends TextWebSocketHandler {
                     t.getName()
             ));
         }
-        message.getPayload().setTemperatureModel(temperatures);
-
-
+        payload.setTemperatureModel(temperatures);
 
         List<ImageModel> images = new ArrayList<>();
         for (Image i : client.getImages()){
@@ -197,8 +226,134 @@ public class WebSocketClientHandler extends TextWebSocketHandler {
         }
         payload.setImageModel(images);
 
+        List<PeripheryModel> peripheries = new ArrayList<>();
+        for (Periphery p : client.getPeripheries()){
+            peripheries.add(new PeripheryModel(
+                p.getOwner(),
+                p.getDate().getTime(),
+                p.getName(),
+                p.getGpioBcm(),
+                p.getStatus()
+            ));
+        }
+        payload.setPeripheryModels(peripheries);
+
         session.sendMessage(new TextMessage(gson.toJson(message)));
         System.out.println(gson.toJson(message));
     }
 
+    private void switchPeripheryStatus(PeripheryModel periphery){
+        Device device = deviceRepository.findBySerialNumber(periphery.getOwner());
+
+        RpiMessage message = new RpiMessage();
+        message.setType("periphery");
+        message.setKey(device.getDeviceKey());
+        Gson gson = new Gson();
+        String json = gson.toJson(message);
+
+        String url = "http://" + device.getIp() + ":8081/test";
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        try {
+            HttpPost request = new HttpPost(url);
+            StringEntity params = new StringEntity(json);
+            request.addHeader("content-type", "application/json");
+            request.setEntity(params);
+            httpClient.execute(request);
+        }
+        catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        finally {
+            try {
+                httpClient.close();
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private void getTemperatureArchiveData(WebSocketSession session, TemperatureModel tModel) throws Exception{
+        Iterable<TemperatureArchive> temps = temperatureArchiveRepository.findAllByOwnerAndAndName(tModel.getOwnerSerialNumber(), tModel.getName());
+        List<TemperatureModel> temperatureModels = new ArrayList<>();
+        for (TemperatureArchive t : temps){
+            temperatureModels.add(new TemperatureModel(
+                    t.getOwner(),
+                    t.getTemp(),
+                    t.getDate().getTime(),
+                    t.getName()
+            ));
+        }
+
+        Payload payload = new Payload();
+        payload.setTemperatureModel(temperatureModels);
+        WsMessage message = new WsMessage();
+        message.setPayload(payload);
+        message.setType("temperatureArchiveData");
+
+        System.out.println(gson.toJson(message));
+        session.sendMessage(new TextMessage(gson.toJson(message)));
+    }
+
+    Runnable checkIfDatabaseRecordChanged = new Runnable() {
+        @Override
+        public void run()  {
+            for (Map.Entry<WebSocketSession, ClientData> entry : sessionClientDataList.entrySet()){
+                List<Device> devices = entry.getValue().getDevices();
+                for (Device d : devices){
+                    Optional<Device> dev = deviceRepository.findById(d.getId());
+                    if (dev.isPresent()){
+                        Iterable<Temperature> temperatures = temperatureRepository.findAllByOwner(dev.get().getSerialNumber());
+                        Iterable<Image> images = imageRepository.findAllByOwner(dev.get().getSerialNumber());
+                        Iterable<Periphery> peripheries = peripheryRepository.findAllByOwner(dev.get().getSerialNumber());
+
+                        List<TemperatureModel> tModels = new ArrayList<>();
+                        List<ImageModel> iModels = new ArrayList<>();
+                        List<PeripheryModel> pModels = new ArrayList<>();
+
+                        for (Temperature t : temperatures){
+                            tModels.add(new TemperatureModel(
+                                    t.getOwner(),
+                                    t.getTemp(),
+                                    t.getDate().getTime(),
+                                    t.getName()
+                            ));
+                        }
+                        for (Image i : images){
+                            iModels.add(new ImageModel(
+                                    i.getOwner(),
+                                    Base64.getEncoder().encodeToString(i.getImage()),
+                                    i.getDate().getTime(),
+                                    i.getName()
+                            ));
+                        }
+                        for (Periphery p : peripheries){
+                            pModels.add(new PeripheryModel(
+                                    p.getOwner(),
+                                    p.getDate().getTime(),
+                                    p.getName(),
+                                    p.getGpioBcm(),
+                                    p.getStatus()
+                            ));
+                        }
+
+                        Payload payload = new Payload();
+                        payload.setTemperatureModel(tModels);
+                        payload.setImageModel(iModels);
+                        payload.setPeripheryModels(pModels);
+
+                        WsMessage message = new WsMessage();
+                        message.setType("runnableSensors");
+                        message.setPayload(payload);
+                        try {
+                            entry.getKey().sendMessage(new TextMessage(gson.toJson(message)));
+                        }
+                        catch (Exception e){
+                        }
+                    }
+                }
+            }
+        }
+    };
 }
